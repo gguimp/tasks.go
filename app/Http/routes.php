ddb268bb7cf3017146c3417f6434889596ac61f4
Gustavo G. P <gguimp@gmail.com>
2016-03-04 20:18:01 -0300

    Adicionado template para index

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +27,7 @@ Route::get('/', function () {
 */
 
 Route::group(['middleware' => ['web']], function () {
-    //
+	Route::get('/', function() {
+		return view('index');
+	});
 });
