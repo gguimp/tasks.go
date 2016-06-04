88583dfc472751e7efadeb5e28e7647010100a4d
Gustavo G. Pinto <gguimp@gmail.com>
2016-06-04 15:43:29 -0300

    Descrição da Tarefa
    Adicionado descrição da tarefa a view "tarefas/novo"

--- resources/views/tarefas/novo.blade.php
+++ resources/views/tarefas/novo.blade.php
@@ -8,9 +8,20 @@
 
             <!-- Tarefa -->
             <div class="form-group">
-                <label for="tarefa-nome">Nova Tarefa</label>
+                <label for="tarefa-nome">Tarefa</label>
                 <input type="text" name="tarefa" id="tarefa-nome" class="form-control">
             </div>
+
+            <!-- Descrição da Tarefa -->
+            <div class="form-group">
+                <label for="tarefa-descricao">Descrição...</label>
+                <textarea name="descricao" id="tarefa-descricao" rows="3" class="form-control">
+                </textarea>
+            </div>
+
+            <!-- Botão salvar. -->
+            <button type="submit" class="btn btn-primary">
+                <i class="fa fa-plus"></i>Salvar</button>
         </form>
     </div>
 </div>
