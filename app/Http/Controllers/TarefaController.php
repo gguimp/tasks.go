a4d01572d9f70f9ed0ac7d8da9548d45f42ec73b
Gustavo G. Pinto <gguimp@gmail.com>
2016-06-11 20:15:07 -0300

    Operações com tarefa.

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -5,16 +5,34 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Tarefa;
+use Debugbar;
 
 class TarefaController extends Controller
 {
+	public function __construct()
+	{
+		$this->middleware("auth");
+	}
+
     public function index(Request $request)
     {
     	$tarefas = Tarefa::all();
 
+			// TODO: Teste de tarefa...
+
     	return view('tarefas.index', [
     		'tarefas' => $tarefas]);
     }
+
+	public function visualizar(Request $request, Tarefa $tarefa)
+	{
+		Debugbar::info('teste de mensagem...');
+		Debugbar::info($tarefa);
+
+		return view('tarefas.visualizar', [
+			'tarefa' => $tarefa
+		]);
+	}
 
     public function novo(Request $request)
     {
